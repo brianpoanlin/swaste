f63bbfae475011b5ced45ca694b17c125cf97a22
Nikos Mouchtaris <nikosm@Nikoss-MacBook-Air.local>
2019-09-07 10:19:29 -0400

    ipad host app start

---
+++ iPadHostApp/iPadHostApp/ProxSensor.h
@@ -0,0 +1,20 @@
+//
+//  ProxSensor.h
+//  iPadHostApp
+//
+//  Created by Nikos Mouchtaris on 9/7/19.
+//  Copyright © 2019 Nikos Mouchtaris. All rights reserved.
+//
+
+#ifndef ProxSensor_h
+#define ProxSensor_h
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+@interface ProxSensor : NSObject
+
+@property bool close;
+
+- (void) someMethod;
+
+@end
+#endif /* ProxSensor_h */
---
+++ iPadHostApp/iPadHostApp/ProximitySensor.swift
@@ -0,0 +1,28 @@
+
+//
+//  ProximitySensor.swift
+//  iPadHostApp
+//
+//  Created by Nikos Mouchtaris on 9/7/19.
+//  Copyright © 2019 Nikos Mouchtaris. All rights reserved.
+//
+
+import UIKit
+
+class ProximitySensor: NSObject {
+//    func setProximitySensorEnabled(_ enabled: Bool) {
+//        let device = UIDevice.current
+//        device.isProximityMonitoringEnabled = enabled
+//        if device.isProximityMonitoringEnabled {
+//            NotificationCenter.default.addObserver(self, selector: #selector(proximityChanged), name: .UIDevice.proximityStateDidChangeNotification, object: device)
+//        } else {
+//            NotificationCenter.default.removeObserver(self, name: .UIDevice.proximityStateDidChangeNotification, object: nil)
+//        }
+//    }
+//    
+//    @objc func proximityChanged(_ notification: Notification) {
+//        if let device = notification.object as? UIDevice {
+//            print("\(device) detected!")
+//        }
+//    }
+}
